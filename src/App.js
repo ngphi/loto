5ce6fa81ae978e63a5b648c057c546ef19a357e0
Phi Nguyen <n.phi1212@gmail.com>
2018-12-29 07:11:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getRandomArbitrary(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
+var voices = window.speechSynthesis.getVoices();
+
+function say(m) {
+  var msg = new SpeechSynthesisUtterance();
+  msg.voice = voices[1];
+  msg.voiceURI = "native";
+  msg.volume = 1;
+  msg.rate = 1;
+  msg.pitch = 0.8;
+  msg.text = m;
+  msg.lang = 'vi-VN';
+  speechSynthesis.speak(msg);
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: []
+    };
+  }
+
+  start = () => {
+    if (!this.inter) {
+      this.inter = setInterval(this.randomNumber, 3000)
+    }
+  }
+
+  stop = () => {
+    clearInterval(this.inter)
+  }
+
+  randomNumber = () => {
+    const number = getRandomArbitrary(1, 90);
+    if (this.state.list.includes(number)) {
+      return this.randomNumber();
+    }
+
+    this.setState({ list: [number, ...this.state.list] }, () => {
+      say(number);
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+              <button onClick={this.stop}>stop</button>
+
+        <button onClick={this.start}>start</button>
+        <ul>
+          {this.state.list.map(v => (
+            <li key={v}>{v}</li>
+          ))}
+        </ul>
       </div>
     );
   }
